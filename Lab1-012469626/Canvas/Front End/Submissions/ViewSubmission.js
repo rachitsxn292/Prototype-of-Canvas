afdb186333b4e076549726806a0dd21667b16c5f
rachitsxn292 <47805866+rachitsxn292@users.noreply.github.com>
2019-03-15 22:25:11 -0700

    Front End Commit

---
+++ Lab1-012469626/Canvas/Front End/Submissions/ViewSubmission.js
@@ -0,0 +1,101 @@
+
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+import cookie from 'react-cookies';
+import {Redirect} from 'react-router';
+import FacultyDashBoard from '../FacultyDashboard/FacultyDashboard';
+
+class ViewSubmission extends Component {
+    constructor(props){
+        super(props)
+            this.state={
+                courseid:cookie.load('courseid'),
+                fileView:[],
+                file: '',
+                marks:''
+               
+            }
+    }
+    componentDidMount()
+    {
+        const {courseid} = this.state;
+        axios.get('http://localhost:3001/ViewAssignmentSubmission', {params: {courseid}})
+        .then(res => {
+            console.log(res.data);
+            this.setState({
+                fileView: this.state.fileView.concat(res.data)
+            })
+        });
+    }
+
+    SubmitMarks()
+    {
+        const {courseid} = this.state;
+    axios.post('http://localhost:3001/assignmentMarks', {
+            marks: this.state.marks,
+            courseid
+        })
+        .then(response=>{
+            console.log("Marks Submitted");
+    });
+    } 
+
+    MarksUpload(event){
+        this.setState({
+            marks:event.target.value
+        })
+    }
+    render() {
+        let redirectVar=null;
+        if(!(cookie.load('email'))){
+            redirectVar=<Redirect to='/'/>
+        }
+        let uploads = this.state.fileView.map(fileViews => {
+            return(
+                <tr key={fileViews.COURSEID}>
+                    <td>{fileViews.COURSEID}</td>
+                    <td>{fileViews.EMAIL}</td>
+                    <td><a style={{WebkitTextFillColor: 'blue', cursor: 'pointer'}} onClick={()=>{
+                        console.log(fileViews.ASSIGNMENTLOCATION);
+                        this.setState({
+                            file: <embed src={fileViews.ASSIGNMENTLOCATION} type="application/pdf" width="500px" height="800px"/>
+                        })
+                    }}>{fileViews.ASSIGNMENTLOCATION}</a></td>
+                    <td><input type="text" id="AssignmentMarks" name="AssignmentMarks" value={this.state.marks} onChange={this.MarksUpload.bind(this)} placeholder="Enter Marks" /></td>
+                </tr>
+           )
+        })
+        return (
+            <div>
+            {redirectVar}
+            <FacultyDashBoard/>
+                <div>
+                
+                </div>
+                    <div id="viewUploadsMain">
+                                <table  className="tableViewLecture" border="0|1">
+                                    <thead>
+                                        <tr>
+                                            <th>Course ID</th>
+                                            <th>Student Email</th>
+                                            <th>Assignment Location</th>
+                                            <th>Marks</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {uploads}
+                                    </tbody>
+                                </table>
+                    </div>
+                {this.state.file}
+                <div>
+                <button type="submit" value="submitMarks" onClick={this.SubmitMarks.bind(this)} name="submitMarks" id="submitMarks" >
+                    Submit Marks</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ViewSubmission;
